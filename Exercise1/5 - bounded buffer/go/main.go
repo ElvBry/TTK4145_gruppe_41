2bf2d4ded8b51011888594cc5116a34b498bdc29
ElvBry <197415608+ElvBry@users.noreply.github.com>
2026-01-14 17:51:02 +0100

    Finished and tested 5- bounded buffer go program

--- Exercise1/5 - bounded buffer/go/main.go
+++ Exercise1/5 - bounded buffer/go/main.go
@@ -1,38 +1,38 @@
-
 package main
 
-import "fmt"
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
+func producer(ch chan int, doneChan chan struct{}) {
 
-func producer(/*TODO: parameters?*/){
-
-    for i := 0; i < 10; i++ {
-        time.Sleep(100 * time.Millisecond)
-        fmt.Printf("[producer]: pushing %d\n", i)
-        // TODO: push real value to buffer
-    }
+	for i := 0; i < 10; i++ {
+		time.Sleep(100 * time.Millisecond)
+		fmt.Printf("[producer]: pushing %d\n", i)
+		ch <- i
+	}
 
 }
 
-func consumer(/*TODO: parameters?*/){
+func consumer(ch chan int) {
 
-    time.Sleep(1 * time.Second)
-    for {
-        i := 0 //TODO: get real value from buffer
-        fmt.Printf("[consumer]: %d\n", i)
-        time.Sleep(50 * time.Millisecond)
-    }
-    
+	time.Sleep(1 * time.Second)
+	for {
+		i := <-ch
+		fmt.Printf("[consumer]: %d\n", i)
+		time.Sleep(50 * time.Millisecond)
+	}
+
 }
 
+func main() {
+	// TODO: make a bounded buffer
+	ch := make(chan int, 5)
+	doneChan := make(chan struct{}, 1)
 
-func main(){
-    
-    // TODO: make a bounded buffer
-    
-    go consumer()
-    go producer()
-    
-    select {}
+	go consumer(ch)
+	go producer(ch, doneChan)
+
+	select {}
 }
